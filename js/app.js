6944b6311c867b515e5ccf5a4e53400a48dc7f90
Nick Shvelidze <captain@pirrate.me>
2014-04-24 17:39:08 +0400

    such google much now

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function(){
+  var updateDateString = function() {
+		var d = new Date();
+		var weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+		var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+		var weekName = weekdays[d.getDay()];
+		var monthName = monthNames[d.getMonth()];
+		var monthDate = d.getDate();
+		var year = d.getFullYear();
+		$(".date").text(weekName + " " + monthDate+" "+monthName+" "+year);
+	}
+
+	var updateTimeString = function(){
+		var nowDate=new Date();
+		var hours=nowDate.getHours();
+		var minutes=nowDate.getMinutes();
+		minutes=minutes>9 ? minutes : "0"+minutes;
+		hours=hours>9 ? hours : "0"+hours;
+		$(".clock").text(hours+":"+minutes);
+	}
+
+	updateTimeString();
+	setInterval(updateTimeString,1000);
+	updateDateString();
+})();
